ab1f9e2d3f0ef3d937e63d74b1c1b58beb56eee3
CaolanRice <103444357+CaolanRice@users.noreply.github.com>
2023-02-11 23:35:41 +0100

    Add files via upload

---
+++ Part-2/part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,12 @@
+
+public class Division {
+
+    public static void main(String[] args) {
+
+        division(3, 5);
+    }
+
+    public static void division(int num, int denom) {
+        System.out.println((double) num / denom);
+    }
+}
