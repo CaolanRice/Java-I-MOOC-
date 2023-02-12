67720ed12e71c023032b30f196fbca55a5bd3d01
CaolanRice <103444357+CaolanRice@users.noreply.github.com>
2023-02-12 21:52:13 +0100

    Add files via upload

---
+++ Part-3/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        while (true){
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1){
+                break;
+            }
+            numbers.add(input);
+        }
+        
+        double sum = 0;
+        for (int number: numbers){
+            sum += number;
+        }
+        double average = sum / numbers.size();
+        System.out.println("Average: " + average);
+
+    }
+}
